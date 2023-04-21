a0511d221e9c224b98f6c93b7541741b2ee1d866
LMO GerritBot <gerrit@libremobileos.com>
2023-04-21 22:30:32 +0000

    Adapt to LMODroid by device-add script
    Change-Id: I7df38b899d236d4190f9b26b6ac268cf0e0c110a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ocean.mk
+    $(LOCAL_DIR)/lmodroid_ocean.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ocean-eng \
-    lineage_ocean-user \
-    lineage_ocean-userdebug
+    lmodroid_ocean-eng \
+    lmodroid_ocean-user \
+    lmodroid_ocean-userdebug
